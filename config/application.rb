c44d7009e91e40f1dd46d7a53516287d9977f66c
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-19 16:18:29 +0900

    First commit

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,18 @@ module MyCurryApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators.template_engine = :slim
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end

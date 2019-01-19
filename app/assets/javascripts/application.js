6efdd3618b3b5f02f5044dd9ea55d091c93371bd
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-19 16:39:10 +0900

    First commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets

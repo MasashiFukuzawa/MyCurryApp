24cd9808069f3544ed20fdae69d30276de32739a
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-21 11:31:08 +0900

    多分消す

---
+++ db/migrate/20190121022813_create_shops.rb
@@ -0,0 +1,19 @@
+class CreateShops < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.string :phone
+      t.string :image
+      t.string :address
+      t.string :area
+      t.string :station
+      t.string :url
+      t.string :twitter_url
+      t.string :facebook_url
+      t.string :tabelog_url
+      t.string :google_map_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/shops.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :shop do
+    name { "MyString" }
+    phone { "MyString" }
+    image { "MyString" }
+    address { "MyString" }
+    area { "MyString" }
+    station { "MyString" }
+    url { "MyString" }
+    twitter_url { "MyString" }
+    facebook_url { "MyString" }
+    tabelog_url { "MyString" }
+    google_map_url { "MyString" }
+  end
+end

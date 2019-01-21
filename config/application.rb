3543bd4cdc6ba68321af36757d54841b26bceca3
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-21 11:49:59 +0900

    Delete Active Storage setting

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'active_strage/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

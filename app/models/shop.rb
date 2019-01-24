22cdf38391a9cd2ec973a7ea942af398d237e82f
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-01-24 18:04:24 +0900

    Tweak header layout

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,11 +1,6 @@
 class Shop < ApplicationRecord
-  validates :name, presence: {message: 'は必須です。'},
-                   length: {maximum: 50, message: '%{value}は50文字以内でご記入下さい。'}
-  validates :phone, presence: {message: 'は必須です。'},
-                    format: {with: /\A\d{10}$|^\d{11}\z/, message: 'ハイフンなしの10桁または11桁でご記入下さい。'},
-                    uniqueness: {message: '%{value}は既に他店で使われています。'}
-  validates :address, presence: {message: 'は必須です。'},
-                      length: {maximum: 100, message: '%{value}は100文字以内でご記入下さい。'}
-  validates :area, :station, presence: {message: 'は必須です。'},
-                             length: {maximum: 20, message: '%{value}は20文字以内でご記入下さい。'}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :phone, presence: true, format: {with: /\A\d{10}$|^\d{11}\z/}, uniqueness: true
+  validates :address, presence: true, length: {maximum: 100}
+  validates :area, :station, presence: true, length: {maximum: 20}
 end

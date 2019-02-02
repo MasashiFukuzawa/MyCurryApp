4c9b0f894729dba7d9cc3118619df43b3869f9a2
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-02-02 22:59:11 +0900

    Tweak flash messages' style for devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
     return base_title if page_title.empty?
     "#{page_title} | #{base_title}"
   end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success", "notice"
+        "success"
+      when "error", "alert"
+        "danger"
+      else
+        flash_type.to_s
+    end
+  end
 end

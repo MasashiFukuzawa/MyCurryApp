e07918f417dad263c4831c44f671e8964ce26563
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-02-04 16:07:06 +0900

    Undo production.rb & bundle update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.initialize_on_precompile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'

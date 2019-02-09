7cac0f049ad5e8eb1069b6bba8aa0794c5e99616
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-02-09 09:35:01 +0900

    Heroku deploy test

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -7,11 +7,13 @@ s3_options = {
   access_key_id:     ENV['S3_ACCESS_KEY_ID'],
   secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
   region:            ENV['S3_REGION'],
-  bucket:            ENV['S3_BUCKET']}
+  bucket:            ENV['S3_BUCKET']
+}
 
 Shrine.storages = {
   cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
+  store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
+}
 
 # 使用するプラグインの宣言
 Shrine.plugin :activerecord

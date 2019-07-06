81c1ba951a7a11a1db3fca9573c75fb1b02983ec
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-07-06 14:29:44 +0900

    Edit setting files

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,16 +3,22 @@ require 'shrine/storage/file_system'
 require 'shrine/storage/s3'
 
 # アップロードするディレクトリの指定
-s3_options = {
-  access_key_id:     ENV['S3_ACCESS_KEY_ID'],
-  secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-  region:            ENV['S3_REGION'],
-  bucket:            ENV['S3_BUCKET']
-}
+# S3代が地味に掛かるため一旦ストレージとのリンクを切ってます（2019.07.08）
+# s3_options = {
+#   access_key_id:     ENV['S3_ACCESS_KEY_ID'],
+#   secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+#   region:            ENV['S3_REGION'],
+#   bucket:            ENV['S3_BUCKET']
+# }
+
+# Shrine.storages = {
+#   cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+#   store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
+# }
 
 Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-  store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)
+  cache: Shrine::Storage::FileSystem.new('public', prefix: 'images/uploads/cache'),
+  store: Shrine::Storage::FileSystem.new('public', prefix: 'images/uploads/store'),
 }
 
 # 使用するプラグインの宣言
